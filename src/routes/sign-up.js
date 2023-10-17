4dcf6da648aa3fa8d94d564e1543a17d3ea52a24
Gaurav <gaurav@cvs.du.ac.in>
2023-10-17 19:00:30 +0530

    little updation 1

---
+++ src/routes/sign-up.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/schema');
+const bcrypt = require('bcrypt');
+
+// POST /signup route for user sign-up
+router.post('/signup', async (req, res) => {
+  const { username, phoneNumber , email , password} = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      phoneNumber, 
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.json({ message: 'User signed up successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred during sign-up.' });
+  }
+});
+
+module.exports = router;
